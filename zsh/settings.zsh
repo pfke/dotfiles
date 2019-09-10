c82d2e9675f132ae8a92ffb7450b6cd337a46ea7
Heiko Blobner <h.blobner@avm.de>
2019-09-10 11:40:40 +0200

    zsh: liquidprompt configured+bootstrap, zplugin bootstrap

--- zsh/settings.zsh
+++ zsh/settings.zsh
@@ -1,8 +1,11 @@
 # Only load Liquid Prompt in interactive shells, not from a script or from scp
-if [[ ! -d ~/.config/liquidprompt  ]]; then
-    git -C ~/.config clone https://github.com/nojhan/liquidprompt.git
+if [[ ! -d ${HOME}/.config/liquidprompt ]]; then
+    git -C ${HOME}/.config clone https://github.com/nojhan/liquidprompt.git
 fi
-[[ $- = *i* ]] && source ~/.config/liquidprompt/liquidprompt
+if [[ ! -e ${HOME}/.config/liquidpromptrc ]]; then
+    ln -s ${PATH_TO_DOTFILES_REPO}/zsh/liquidpromptrc ${HOME}/.config/liquidpromptrc
+fi
+[[ $- = *i* ]] && source ${HOME}/.config/liquidprompt/liquidprompt
 
 export EDITOR="nvim"
 export VISUAL=$EDITOR
@@ -14,3 +17,9 @@ setopt appendhistory autocd extendedglob nomatch
 unsetopt beep notify
 bindkey -e
 
+# The following lines were added by compinstall
+zstyle :compinstall filename "${HOME}/.zshrc"
+
+autoload -Uz compinit
+compinit
+# End of lines added by compinstall
